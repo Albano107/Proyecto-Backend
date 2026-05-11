b6c7f450ac7221c47bb907b4869058e4d4a5e593
albano martinez <albano_107@hotmail.com>
2026-05-11 09:50:22 -0300

    Configuracion inicial backend

---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+import sql from 'mssql/msnodesqlv8.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config = {
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
+    driver: process.env.DB_DRIVER,
+    options: {
+        trustedConnection: true
+    }
+};
+
+export const connectDB = async () => {
+    try {
+        await sql.connect(config);
+        console.log('Conectado a SQL Server');
+    } catch (error) {
+        console.error('Error de conexión:', error);
+    }
+};
+
+export default sql;
