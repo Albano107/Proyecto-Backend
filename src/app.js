ce0d4b6a76f0bd239461d56d659f1a6edc8c1bcc
albano martinez <albano_107@hotmail.com>
2026-06-01 20:44:21 -0300

    Endpoint de productos funcionando

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import productosRoutes from './routes/productos.routes.js';
+
 dotenv.config();
 
 const app = express();
@@ -14,5 +16,8 @@ app.get('/', (req, res) => {
         mensaje: 'Backend funcionando correctamente'
     });
 });
+
+// Rutas
+app.use('/productos', productosRoutes);
 
 export default app;
